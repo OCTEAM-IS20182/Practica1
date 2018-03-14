440e787c923ec1902eab381579b7c08b0adfae7f
OrdepZehcnas <pedro_merolito@hotmail.com>
2018-03-14 13:36:43 -0600

    cambiando la tabla Registro por Usuario

--- SQL/proyecto-servicio-social.sql
+++ SQL/proyecto-servicio-social.sql
@@ -5,14 +5,13 @@ Autor: Pedro Juan Salvador Sanchez Perez
 Rol: Representante del Equipo y Desarrollador
 */
 
-CREATE TABLE Registro(
-   USUARIO						TEXT PRIMARY KEY     NOT NULL,
+CREATE TABLE Usuario(
+   ID							INT  PRIMARY KEY,
+   USUARIO						TEXT NOT NULL UNIQUE,
    CORREO_ELECTRONICO           TEXT    NOT NULL,
-   CORREO_CONFIRMACION          TEXT     NOT NULL,
    NOMBRE			            TEXT     NOT NULL,
    APELLIDO			            TEXT     NOT NULL,
    CONTRASEÑA		            TEXT     NOT NULL,
-   CONTRASEÑA_CONFIRMACION      TEXT     NOT NULL,
    FECHA_DE_NACIMIENTO          DATE,
    TELEFONO			            INT,
    CARRERA 			 	        CHAR(50)
